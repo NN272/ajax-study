27f11eea5c538f1db87388b7089d267afe2b9f69
d0tc0m-exe <vey.the.user@gmail.com>
2020-10-26 23:16:20 +0200

    get, query-params

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-$.ajax('https://repetitora.net/api/JS/Images', {
-    success: function(data) {
-        data.forEach(el => {
-            const img = document.createElement('img');
-            img.src = el.thumbnail;
-            document.querySelector('body').appendChild(img);
-        });
-    }
-});
+const button = document.querySelector("#click-me");
+const result = document.querySelector("#result");
+const input = document.querySelector("#input");
+button.addEventListener("click", makeRequest);
+
+function makeRequest() {
+    $.ajax(`https://repetitora.net/api/JS/Images?page=${input.value}&count=1`, {
+        success: function(data) {
+            data.forEach(el => {
+                const img = document.createElement('img');
+                img.src = el.thumbnail;
+                document.querySelector('#result').appendChild(img);
+            });
+        }
+    });
+}
