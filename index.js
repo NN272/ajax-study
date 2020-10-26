d07024d4966c78a4bf12e33d95c7e7cc61b8a5e3
d0tc0m-exe <vey.the.user@gmail.com>
2020-10-26 21:10:33 +0200

    success, callback

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-$.ajax('https://repetitora.net/api/JS/Images');
+$.ajax('https://repetitora.net/api/JS/Images', {
+    success: function(data) {
+        data.forEach(el => {
+            const img = document.createElement('img');
+            img.src = el.thumbnail;
+            document.querySelector('body').appendChild(img);
+        });
+    }
+});
