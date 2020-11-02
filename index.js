bc71de4c258ed45001792df4badab0cc0f0f53eb
d0tc0m-exe <vey.the.user@gmail.com>
2020-11-02 11:34:38 +0200

    todo-list

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const button = document.querySelector("#click-me");
 const result = document.querySelector("#result");
 const input = document.querySelector("#input");
+const createinput = document.querySelector("#create-input");
+const createbutton = document.querySelector("#create-tasks");
+const res = document.querySelector("#res");
+const getTsks = document.querySelector("#get-tasks");
+
 button.addEventListener("click", () => {
     const promise = makeRequest(input.value);
     promise
         .then(onDataRecieved);
+});
+
+createbutton.addEventListener("click", () => {
+    const promise = createTasks(createinput.value);
+});
+
+getTsks.addEventListener("click", () => {
+    const promise = getTasks();
+    promise.then(onTasksRecieved);
 });
 
 function onDataRecieved(data) {
@@ -12,5 +26,13 @@ function onDataRecieved(data) {
         const img = document.createElement('img');
         img.src = el.thumbnail;
         document.querySelector('#result').appendChild(img);
+    });
+}
+
+function onTasksRecieved(tasks) {
+    tasks.forEach(task => {
+        const li = document.createElement('li');
+        li.innerHTML = task.title;
+        document.querySelector('#res').appendChild(li);
     });
 }
--- service.js
+++ service.js
@@ -5,3 +5,19 @@ function makeRequest(page) {
         return data.data;
     });
 }
+
+function getTasks() {
+    const tasks = axios.get(`https://repetitora.net/api/JS/Tasks?widgetId=5345235`);
+
+    return tasks.then((response) => {
+        return response.data;
+    });
+}
+
+function createTasks(title) {
+    const create = axios.post(`https://repetitora.net/api/JS/Tasks?widgetId=5345235&title=${title}`);
+
+    return create.then((response) => {
+        return response.data;
+    });
+}
